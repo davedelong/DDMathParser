924999cf49f5545884b42bd37d4e31e866cce9c8
Zorg <zorgiepoo@gmail.com>
2014-03-15 15:39:47 -0400

    Fixed median not taking correct average for even # of arguments Also, ceil and floor are not necessary. Found this code thanks to -Wconversion

--- DDMathParser/_DDFunctionEvaluator.m
+++ DDMathParser/_DDFunctionEvaluator.m
@@ -345,11 +345,11 @@ static NSString *const _DDFunctionSelectorSuffix = @":variables:error:";
 	
 	NSNumber *median = nil;
 	if (([evaluatedNumbers count] % 2) == 1) {
-		NSUInteger index = floor([evaluatedNumbers count] / 2);
+		NSUInteger index = [evaluatedNumbers count] / 2;
 		median = [evaluatedNumbers objectAtIndex:index];
 	} else {
-		NSUInteger lowIndex = floor([evaluatedNumbers count] / 2);
-		NSUInteger highIndex = ceil([evaluatedNumbers count] / 2);
+		NSUInteger highIndex = [evaluatedNumbers count] / 2;
+		NSUInteger lowIndex = highIndex - 1;
         NSNumber *low = [evaluatedNumbers objectAtIndex:lowIndex];
         NSNumber *high = [evaluatedNumbers objectAtIndex:highIndex];
         median = @(([low doubleValue] + [high doubleValue])/2);
