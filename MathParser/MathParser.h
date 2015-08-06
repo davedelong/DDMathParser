38d5292d5aac5ebdb5b702848603d68acd006d31
Dave DeLong <davedelong@me.com>
2015-08-06 14:17:03 -0700

    Add new empty MathParser target

---
+++ MathParser/MathParser.h
@@ -0,0 +1,19 @@
+//
+//  MathParser.h
+//  MathParser
+//
+//  Created by Dave DeLong on 8/6/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MathParser.
+FOUNDATION_EXPORT double MathParserVersionNumber;
+
+//! Project version string for MathParser.
+FOUNDATION_EXPORT const unsigned char MathParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MathParser/PublicHeader.h>
+
+
