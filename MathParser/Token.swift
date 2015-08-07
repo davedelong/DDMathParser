95d59e60491cce73aff7e10fbcc7ac5cbce281ab
Dave DeLong <davedelong@me.com>
2015-08-07 10:18:48 -0700

    Add in a basic Token type
    We still don't know what KIND of token it is (operator, function, number, etc), but it's a start

---
+++ MathParser/Token.swift
@@ -0,0 +1,15 @@
+//
+//  Token.swift
+//  DDMathParser
+//
+//  Created by Dave DeLong on 8/6/15.
+//
+//
+
+import Foundation
+
+public struct Token {
+    public let string: String
+    public let sourceRange: Range<String.CharacterView.Index>
+    
+}
