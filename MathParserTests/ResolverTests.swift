d04da879b0b8f10f49e954bb26c2324a23822630
Dave DeLong <davedelong@me.com>
2015-08-13 11:48:57 -0700

    Operator disambiguation tests

--- MathParserTests/ResolverTests.swift
+++ MathParserTests/ResolverTests.swift
@@ -16,7 +16,7 @@ class TokenResolverTests: XCTestCase {
         let tokens = XCTAssertNoThrows(try r.resolve())
         
         XCTAssertEqual(tokens?.count, 1)
-        TestToken(tokens?[0], kind: ResolvedTokenKind.Number(1), string: "1")
+        TestToken(tokens?[0], kind: .Number(1), string: "1")
     }
     
     func testHexNumber() {
@@ -24,7 +24,7 @@ class TokenResolverTests: XCTestCase {
         let tokens = XCTAssertNoThrows(try r.resolve())
         
         XCTAssertEqual(tokens?.count, 1)
-        TestToken(tokens?[0], kind: ResolvedTokenKind.Number(16), string: "10")
+        TestToken(tokens?[0], kind: .Number(16), string: "10")
     }
     
     func testVariable() {
@@ -32,7 +32,7 @@ class TokenResolverTests: XCTestCase {
         let tokens = XCTAssertNoThrows(try r.resolve())
         
         XCTAssertEqual(tokens?.count, 1)
-        TestToken(tokens?[0], kind: ResolvedTokenKind.Variable("foo"), string: "foo")
+        TestToken(tokens?[0], kind: .Variable("foo"), string: "foo")
     }
     
     func testIdentifier() {
@@ -40,7 +40,92 @@ class TokenResolverTests: XCTestCase {
         let tokens = XCTAssertNoThrows(try r.resolve())
         
         XCTAssertEqual(tokens?.count, 1)
-        TestToken(tokens?[0], kind: ResolvedTokenKind.Identifier("foo"), string: "foo")
+        TestToken(tokens?[0], kind: .Identifier("foo"), string: "foo")
+    }
+    
+    func testSimpleOperator() {
+        let r = TokenResolver(string: "+")
+        let tokens = XCTAssertNoThrows(try r.resolve())
+        
+        XCTAssertEqual(tokens?.count, 1)
+        
+        let op = Operator(builtInOperator: .UnaryPlus)
+        TestToken(tokens?[0], kind: .Operator(op), string: "+")
+    }
+    
+    func testUnambiguousOperator() {
+        let r = TokenResolver(string: "^")
+        let tokens = XCTAssertNoThrows(try r.resolve())
+        
+        XCTAssertEqual(tokens?.count, 1)
+        
+        let op = Operator(builtInOperator: .BitwiseXor)
+        TestToken(tokens?[0], kind: .Operator(op), string: "^")
+    }
+    
+    func testOperatorDisambiguation1() {
+        let r = TokenResolver(string: "++")
+        let tokens = XCTAssertNoThrows(try r.resolve())
+        
+        XCTAssertEqual(tokens?.count, 2)
+        
+        let op = Operator(builtInOperator: .UnaryPlus)
+        TestToken(tokens?[0], kind: .Operator(op), string: "+")
+        TestToken(tokens?[1], kind: .Operator(op), string: "+")
+    }
+    
+    func testOperatorDisambiguation2() {
+        let r = TokenResolver(string: "1+")
+        let tokens = XCTAssertNoThrows(try r.resolve())
+        
+        XCTAssertEqual(tokens?.count, 2)
+        
+        let op = Operator(builtInOperator: .Add)
+        TestToken(tokens?[0], kind: .Number(1), string: "1")
+        TestToken(tokens?[1], kind: .Operator(op), string: "+")
+    }
+    
+    func testOperatorDisambiguation3() {
+        let r = TokenResolver(string: "1 2 !", options: [])
+        let tokens = XCTAssertNoThrows(try r.resolve())
+        
+        XCTAssertEqual(tokens?.count, 3)
+        
+        TestToken(tokens?[0], kind: .Number(1), string: "1")
+        TestToken(tokens?[1], kind: .Number(2), string: "2")
+        
+        let fac = Operator(builtInOperator: .Factorial)
+        TestToken(tokens?[2], kind: .Operator(fac), string: "!")
+    }
+    
+    func testOperatorDisambiguation4() {
+        let r = TokenResolver(string: "1°!")
+        let tokens = XCTAssertNoThrows(try r.resolve())
+        
+        XCTAssertEqual(tokens?.count, 3)
+        
+        TestToken(tokens?[0], kind: .Number(1), string: "1")
+        
+        let deg = Operator(builtInOperator: .Degree)
+        TestToken(tokens?[1], kind: .Operator(deg), string: "°")
+        
+        let fac = Operator(builtInOperator: .Factorial)
+        TestToken(tokens?[2], kind: .Operator(fac), string: "!")
+    }
+    
+    func testOperatorDisambiguation5() {
+        let r = TokenResolver(string: "1°+")
+        let tokens = XCTAssertNoThrows(try r.resolve())
+        
+        XCTAssertEqual(tokens?.count, 3)
+        
+        TestToken(tokens?[0], kind: .Number(1), string: "1")
+        
+        let deg = Operator(builtInOperator: .Degree)
+        TestToken(tokens?[1], kind: .Operator(deg), string: "°")
+        
+        let fac = Operator(builtInOperator: .Add)
+        TestToken(tokens?[2], kind: .Operator(fac), string: "+")
     }
 
 }
