8fd357a6f5955a546bd2d378faae035ded867bbc
Matthias Hochgatterer <matthias.hochgatterer@gmail.com>
2018-06-18 11:18:56 +0200

    Support localized decimal numbers with no leading zero

--- MathParser/Tests/MathParserTests/TokenizerTests.swift
+++ MathParser/Tests/MathParserTests/TokenizerTests.swift
@@ -278,6 +278,18 @@ class TokenizerTests: XCTestCase {
         TestToken(tokens[8], kind: LocalizedNumberToken.self, string: "7,8")
         TestToken(tokens[9], kind: OperatorToken.self, string: ")")
     }
+        
+    func testLocalizedNumberWithoutLeadingZero() throws {
+        let evaluator = Evaluator()
+        let locale = Locale(identifier: "de_AT")
+        do {
+            let exp = try Expression(string: ",2", locale: locale)
+            let result = try evaluator.evaluate(exp)
+            XCTAssertEqual(result, 0.2)
+        } catch let error {
+            XCTFail(error.localizedDescription)
+        }
+    }
     
     func testZeroLengthVariables() {
         var c = Configuration.default
